f4c595f14a1410f6f20de530ca10027b3dc2669f
Sivasagar Palakurthy <siva.palakurthy@amzurtech.com>
2019-05-08 15:49:38 +0530

    Pod Inittial commit

---
+++ MockFramwork/MockFramwork/MockFramwork.h
@@ -0,0 +1,19 @@
+//
+//  MockFramwork.h
+//  MockFramwork
+//
+//  Created by Siva Sagar Palakurthy on 08/05/19.
+//  Copyright © 2019 amzurtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MockFramwork.
+FOUNDATION_EXPORT double MockFramworkVersionNumber;
+
+//! Project version string for MockFramwork.
+FOUNDATION_EXPORT const unsigned char MockFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockFramwork/PublicHeader.h>
+
+
