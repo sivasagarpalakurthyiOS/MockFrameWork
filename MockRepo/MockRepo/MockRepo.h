b62d37c3766766f9f169120123c0a8ecc8a7905a
Sivasagar Palakurthy <siva.palakurthy@amzurtech.com>
2019-05-08 16:13:34 +0530

    Mock New Repository

---
+++ MockRepo/MockRepo/MockRepo.h
@@ -0,0 +1,19 @@
+//
+//  MockRepo.h
+//  MockRepo
+//
+//  Created by Siva Sagar Palakurthy on 08/05/19.
+//  Copyright © 2019 amzurtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MockRepo.
+FOUNDATION_EXPORT double MockRepoVersionNumber;
+
+//! Project version string for MockRepo.
+FOUNDATION_EXPORT const unsigned char MockRepoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockRepo/PublicHeader.h>
+
+
