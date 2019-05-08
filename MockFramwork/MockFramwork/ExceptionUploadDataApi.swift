740dae725701570cc54a6ffdb36626c7c5e7ad52
Sivasagar Palakurthy <siva.palakurthy@amzurtech.com>
2019-05-08 16:11:59 +0530

    Repo Source deleted

--- MockFramwork/MockFramwork/ExceptionUploadDataApi.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ExceptionUploadDataApi.swift
-//  MockFramwork
-//
-//  Created by Siva Sagar Palakurthy on 08/05/19.
-//  Copyright © 2019 amzurtech. All rights reserved.
-//
-
-import Foundation
--- MockFramwork/MockFramwork/StubAuthenticationApi.swift
+++
@@ -1,9 +0,0 @@
-//
-//  StubAuthenticationApi.swift
-//  MockFramwork
-//
-//  Created by Siva Sagar Palakurthy on 08/05/19.
-//  Copyright © 2019 amzurtech. All rights reserved.
-//
-
-import Foundation
